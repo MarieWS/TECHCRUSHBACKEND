6467dc2ba9540b0346b3ec0d855fce410d52e604
JPraiseOfficial <jimmypraiseofficial@gmail.com>
2024-12-20 12:08:09 +0100

    changed variable name of database to mysqldb

--- src/config/mysqldb.js
+++ src/config/mysqldb.js
@@ -6,16 +6,16 @@ let databaseName = process.env.DB_NAME
 let username = process.env.DB_USER
 let password = process.env.DB_PASS
 
-const database = new Sequelize(databaseName, username, password, {
+const mysqldb = new Sequelize(databaseName, username, password, {
   host: process.env.DB_HOST,
   dialect: 'mysql'
 });
 
 export const connectMySQL = async () => {
   try {
-    await database.authenticate();
+    await mysqldb.authenticate();
     console.log('Connection has been established successfully.');
-    await database.sync()
+    await mysqldb.sync()
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
