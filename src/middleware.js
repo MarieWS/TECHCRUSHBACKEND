78fd5d807387c9351454b8fecbba93dab188416d
JPraiseOfficial <jimmypraiseofficial@gmail.com>
2024-12-23 16:58:10 +0100

    added more validations

--- src/middleware.js
+++ src/middleware.js
@@ -1,22 +1,53 @@
 import { body, validationResult } from "express-validator";
+import User from "./models.js";
+
+// Custom validations
+let validateIfEmailExists = async (value) => {
+    const existingEmail = await User.findOne({ where: { email: value } })
+
+    if (existingEmail) {
+        throw new Error("Email already exists!");
+    } else {
+        return true
+    }
+}
+
+let validateIfPhoneNumberExists = async (value) => {
+    const existingNumber = await User.findOne({ where: { phoneNumber: value } })
+
+    if (existingNumber) {
+        throw new Error("Phone Number already exists!");
+    } else {
+        return true
+    }
+}
+
+let confirmPassword = (value, { req }) => {
+    if (value !== req.body.password) {
+        throw new Error("Password does not match");
+    } else {
+        return true;
+    }
+}
 
 export const validateForm = [
-    body('firstname').isString().notEmpty().withMessage('Please, input your first name'),
-    body('lastname').isString().notEmpty().withMessage('Please, input your last name'),
-    body('username').isString().notEmpty().withMessage('Please, input your username'),
-    body('phoneNumber').isNumeric().notEmpty().withMessage('Provide a valid phone number'),
-    body('email').isEmail().notEmpty().withMessage('Please, provide a valid email address'),
+    body('firstname')
+        .isString().notEmpty().withMessage('Please, input your first name'),
+    body('lastname')
+        .isString().notEmpty().withMessage('Please, input your last name'),
+    body('username')
+        .isString().notEmpty().withMessage('Please, input your username'),
+    body('phoneNumber')
+        .isNumeric().notEmpty().isLength({ min: 11, max: 11 }).withMessage('Provide a valid phone number')
+        .custom(validateIfPhoneNumberExists),
+    body('email')
+        .isEmail().notEmpty().withMessage('Please, provide a valid email address')
+        .custom(validateIfEmailExists),
     body('password')
         .notEmpty().withMessage('Password is required')
         .isLength({ min: 6 }).withMessage('Password must be at least 5 characters'),
     body('confirmPassword')
-        .custom((value, { req }) => {
-            if (value !== req.body.password) {
-                throw new Error("Password does not match");
-            } else {
-                return true;
-            }
-        }),
+        .custom(confirmPassword),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
