206803a34b61ee2bf8fccfcc3e983639a2aabf05
JPraiseOfficial <jimmypraiseofficial@gmail.com>
2024-12-24 23:03:26 +0100

    created model for user profile

--- src/models.js
+++ src/models.js
@@ -13,3 +13,24 @@ export const User = mysqldb.define(
     },
     {timestamps: true}
 )
+
+export const UserProfile = mysqldb.define(
+    'User_Profile',
+    {
+        gender: {type: DataTypes.STRING, allowNull: false},
+        age: {type: DataTypes.STRING, allowNull: false},
+        country: {type: DataTypes.STRING, allowNull: false},
+        region: {type: DataTypes.STRING, allowNull: false},
+        dietary_preferences: {type: DataTypes.STRING, allowNull: false},
+        health_goals: {type: DataTypes.STRING, allowNull: false},
+        activity_levels: {type: DataTypes.STRING, allowNull: false},
+        allergies: {type: DataTypes.STRING, allowNull: false},
+        medical_condition: {type: DataTypes.STRING, allowNull: false},
+        height: {type: DataTypes.STRING, allowNull: false},
+        weight: {type: DataTypes.STRING, allowNull: false},
+    },
+    {timestamps: true}
+)
+
+User.hasOne(UserProfile);
+UserProfile.belongsTo(User);
--- src/services.js
+++ src/services.js
@@ -1,6 +1,7 @@
-import {User} from "./models.js";
+import {User, UserProfile } from "./models.js";
 
 export const createNewUser = async (firstname, lastname, username, phone_number, email, password) => {
     const user = User.create({firstname, lastname, username, phone_number, email, password});
 }
 
+export const createNewUserProfile = async ()
