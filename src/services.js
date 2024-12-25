1fcb5895d20ba8ea0bd24bd9c045bcf07229fe75
JPraiseOfficial <jimmypraiseofficial@gmail.com>
2024-12-25 18:47:21 +0100

    added bcrypt to encrypt password

--- src/services.js
+++ src/services.js
@@ -1,7 +1,9 @@
 import {User, UserProfile } from "./models.js";
+import bcrypt from 'bcryptjs';
 
 export const createNewUser = async (firstname, lastname, username, phone_number, email, password) => {
-    const user = User.create({firstname, lastname, username, phone_number, email, password});
+    const passwordHash = bcrypt.hashSync(password, 10)
+    const user = User.create({firstname, lastname, username, phone_number, email, password: passwordHash});
 }
 
-export const createNewUserProfile = async ()
+// export const createNewUserProfile = async ()
