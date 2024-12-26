509d8a46ffb9794c2dfdd9be82c47c36163b2b14
JPraiseOfficial <jimmypraiseofficial@gmail.com>
2024-12-26 15:18:32 +0100

    renamed middleware.js and added it to middleware folder

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import * as controllers from './controllers/userController.js';
-import { validateForm } from "./middleware.js";
+import { validateForm } from "./middleware/formValidation.js";
 import { login } from "./controllers/authController.js";
 
 const router = express.Router();
