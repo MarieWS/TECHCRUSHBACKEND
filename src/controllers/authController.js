3360ba7e349a26d746b66288a288ad87d6c00b55
JPraiseOfficial <jimmypraiseofficial@gmail.com>
2024-12-26 14:12:01 +0100

    added login authentication and route

---
+++ src/controllers/authController.js
@@ -0,0 +1,34 @@
+import { User } from "../models.js";
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
+
+const generateToken = (id) => {
+    return jwt.sign(
+        { userID: id }, process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXPIRATION })
+}
+
+export const login = async (req, res) => {
+    const { emailOrPhone, password } = req.body;
+
+    // check if field is empty
+    if (!emailOrPhone || !password) {
+        return res.status(400).json({ message: "Please, fill in the required details" });
+    }
+
+    // check whether user used email or phone number
+    let user
+    if (isNaN(emailOrPhone)) {
+        user = await User.findOne({ where: { email: emailOrPhone } });
+    } else {
+        user = await User.findOne({ where: { phone_number: emailOrPhone } });
+    }
+
+    // check if password matches email/phone number
+    if (!user || !bcrypt.compare(password, user.password)) {
+        return res.status(400).json({ message: "Invalid email or password" });
+    } else {
+        const token = generateToken(user.id)
+        return res.status(200).json({ message: "Login successful", token })
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import * as controllers from './controllers.js';
 import { validateForm } from "./middleware.js";
+import { login } from "./controllers/authController.js";
 
 const router = express.Router();
 
 router.post('/register', validateForm, controllers.createNewUser);
+router.post('/login', login);
 
 export default router
 
