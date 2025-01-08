69ce3654583f5f5eb84c54e7541968dd68a32b3e
Jimmy Praise <93206346+JPraiseOfficial@users.noreply.github.com>
2025-01-08 18:12:11 +0100

    edited cors configuration

--- src/app.js
+++ src/app.js
@@ -12,7 +12,11 @@ import { connectMongoDB } from "./config/mongodb.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',  
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  
+    preflightContinue: false,  
+    optionsSuccessStatus: 204  ));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
  
