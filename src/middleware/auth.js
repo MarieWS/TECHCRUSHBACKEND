4492cfdf722e092621a121b617d44b5d6b33adf3
JPraiseOfficial <jimmypraiseofficial@gmail.com>
2025-01-10 15:14:06 +0100

    wrapped the whole code in try-block

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,16 +3,16 @@ import jwt from "jsonwebtoken"
 const auth = async (req, res, next) => {
     const token = req.cookies.jwtToken;
 
-    if (!token) {
-        res.status(401).json({message: "Access denied"})
-    }
-
     try {
+        if (!token) {
+            return res.status(401).json({ message: "Access denied" })
+        }
+
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decodedToken.userID;
         next();
     } catch (error) {
-        res.status(400).json({message: error.message})
+        res.status(400).json({ message: error.message })
     }
 }
 
